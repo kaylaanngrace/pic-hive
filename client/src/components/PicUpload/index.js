2c59903806ebc22e78bdf5c6c3e7c9e10c4b559a
rharris529 <jordandine@gmail.com>
2022-05-31 19:56:05 -0500

    reviewing pull request

--- client/src/components/PicUpload/index.js
+++ client/src/components/PicUpload/index.js
@@ -7,6 +7,9 @@ const picUpload = () => {
     const uploadImage = (files) => {
         const formData = new FormData()
         formData.append('file', files)
+        formData.append("upload_preset", process.env.CLOUDINARY_UPLOAD_PRESET)
+
+        Axios.post(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/`)
     };
 
     return (
