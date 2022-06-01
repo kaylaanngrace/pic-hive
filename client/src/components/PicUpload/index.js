99ff1e3b3a2665fcd53a03c48bcc8350fad7a1fc
rharris529 <jordandine@gmail.com>
2022-05-31 23:38:33 -0500

    fixed useState issue

--- client/src/components/PicUpload/index.js
+++ client/src/components/PicUpload/index.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import Axios from 'axios'
+import {Image} from 'cloudinary-react';
 
 
-const picUpload = () => {
+const PicUpload = () => {
 
-    const uploadImage = (files) => {
+    const [imageSelected, setImageSelected] = useState("");
+
+    const uploadImage = () => {
         const formData = new FormData()
-        formData.append('file', files)
+        formData.append('file', imageSelected)
         formData.append("upload_preset", process.env.CLOUDINARY_UPLOAD_PRESET)
 
-        Axios.post(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/`)
+        Axios.post(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`, formData)
+        .then((response)=> {
+            console.log(response)
+        });
     };
 
     return (
         <div>
             <input type="file" onChange={(e)=>{
-                uploadImage(e.target.files);
+                setImageSelected(e.target.files[0])
             }}/>
+            <button onClick={uploadImage}>Upload Image</button>
+
+            <Image cloudName="dsx7vhge4" publicId="" />
         </div>
     )
 }
 
-export default picUpload;
+export default PicUpload;
