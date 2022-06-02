85b1178c0b4d6c2e75271d9093eb39927f4f1a63
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-06-02 13:50:57 -0500

    edited color css

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -23,7 +23,7 @@ const Footer = () => {
             aria-label="heart"
             aria-hidden="false"
           >
-            ❤️
+            💛
           </span>{' '}
           by the Pic Hive team.
         </h4>
